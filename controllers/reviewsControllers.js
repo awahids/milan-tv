7842fa86f9df0eb32bc373ec9c78cbe7b04de2c7
ikiqqq <riskidarliana76@gmail.com>
2021-10-01 10:13:31 +0700

    + rating

--- controllers/reviewsControllers.js
+++ controllers/reviewsControllers.js
@@ -1,15 +1,23 @@
-const { Reviews, Users, Movies } = require('../models');
+const Reviews = require('../models/review');
+const Users = require('../models/users');
+const Movies = require('../models/movies')
 const Joi = require('joi');
 
+const sequelize = require('sequelize')
 
 module.exports = {
     postReview: async (req, res) => {
-        const body = req.body
+        const body = req.bod
+        const moviesId = req.params.id
+        const user = req.Users
+
         try {
             const schema = Joi.object({
-                comment: Joi.string(),
                 rating: Joi.number().min(1).max(5).required(),
-            });
+                comment: Joi.string(),
+                usersId: Joi.number(),
+                moviesId: Joi.number()
+            })
 
             const { error } = schema.validate({ ...body }, { abortEarly: false });
 
@@ -20,12 +28,20 @@ module.exports = {
                     errors: error["details"][0]["message"]
                 })
             }
-            const checkUsers = await Reviews.findOne({ where: { UserId: Users.id, MovieId: Movies.id } })
+
+            const checkUsers = await Reviews.findOne({
+                where: {
+                    usersId: body.usersId,
+                    moviesId: body.moviesId
+                }
+            })
+
             if (checkUsers) {
-                return res.status(400).json({
+                return res.status(200).json({
                     status: "Failed",
                     message: "You already add review"
-                })
+
+                });
             } else if (!checkUsers) {
                 return res.status(400).json({
                     status: "Failed",
@@ -34,144 +50,49 @@ module.exports = {
             }
 
             const newReview = await Reviews.create({
+                rating: body.rating,
                 comment: body.comment,
-                rating: body.rating
-            })
-            /* real time */
-            //         appendToDom(data)
-            //         textarea.value = ''
-            //         // Broadcast
-            //         broadcastComment(data)
-            //         // Sync with Mongo Db
-            //         syncWithDb(data)
+                usersId : user.id,
+                moviesId :body.moviesId
+            });
 
-
-            //         function appendToDom(data) {
-            //             let lTag = document.createElement('li')
-            //             lTag.classList.add('comment', 'mb-3')
-
-            //             let markup = `
-            //                     <div class="card border-light mb-3">
-            //                         <div class="card-body">
-            //                             <h6>${data.username}</h6>
-            //                             <p>${data.comment}</p>
-            //                             <div>
-            //                                 <img src="/img/clock.png" alt="clock">
-            //                                 <small>${moment(data.time).format('LT')}</small>
-            //                             </div>
-            //                         </div>
-            //                     </div>
-            // `
-            //             lTag.innerHTML = markup
-
-            //             commentBox.prepend(lTag)
-            //         }
-
-            //         function broadcastComment(data) {
-            //             // Socket
-            //             socket.emit('comment', data)
-            //         }
-
-            //         socket.on('comment', (data) => {
-            //             appendToDom(data)
-            //         })
-            //         let timerId = null
-            //         function debounce(func, timer) {
-            //             if (timerId) {
-            //                 clearTimeout(timerId)
-            //             }
-            //             timerId = setTimeout(() => {
-            //                 func()
-            //             }, timer)
-            //         }
-            //         let typingDiv = document.querySelector('.typing')
-            //         socket.on('typing', (data) => {
-            //             typingDiv.innerText = `${data.username} is typing...`
-            //             debounce(function () {
-            //                 typingDiv.innerText = ''
-            //             }, 1000)
-            //         })
-
-            //         // Event listner on textarea
-            //         textarea.addEventListener('keyup', (e) => {
-            //             socket.emit('typing', { username })
-            //         })
-
-            //         // Api calls 
-
-            //         function syncWithDb(data) {
-            //             const headers = {
-            //                 'Content-Type': 'application/json'
-            //             }
-            //             fetch('/api/comments', { method: 'Post', body: JSON.stringify(data), headers })
-            //                 .then(response => response.json())
-            //                 .then(result => {
-            //                     console.log(result)
-            //                 })
-            //         }
-
-            //         function fetchComments() {
-            //             fetch('/api/comments')
-            //                 .then(res => res.json())
-            //                 .then(result => {
-            //                     result.forEach((comment) => {
-            //                         comment.time = comment.createdAt
-            //                         appendToDom(comment)
-            //                     })
-            //                 })
-            //         }
-
-            //         window.onload = fetchComments
-
-
-            /* average rating */
-            const avarageRating = await Reviews.findAll({
-                where: {
-                    MovieId: Movies.id
+            const allRating = await Reviews.findAll({
+                where : {
+                    moviesId : moviesId
                 }
             })
 
-            let average = avarageRating.map(e => {
+            let ratingAverage = allRating.map(e => {
                 return e.dataValues.rating
             })
-            average.push(body.rating)
 
-            const sum = average.reduce((a, b) => a + b)
-            const realRating = Math.round(sum / average.length)
+            ratingAverage.push(body.rating)
 
-            const updateMovies = await Movies.update({
+            const sum = ratingAverage.reduce((a,b) => a+b)
+            const ratingFix = Math.round(sum / ratingAverage.length)
+
+            const newMovie = await Movies.update({
                 ...body,
-                rating: realRating,
+                rating: ratingFix,
             }, {
                 where: {
-                    id: newReview.dataValues.MovieId
+                    moviesId : mov
                 }
             })
 
-            if (!updateMovies[0]) {
-                transaction.rollback()
+            if (!newMovie[0]) {
                 return res.status(400).json({
-                    status: "Failed",
+                    status: "failed",
                     message: "Unable to update database",
                 });
             }
 
-            newReview.save().then(response => {
-                res.send(response)
-            })
-
-            if (!newReview) {
-                return res.status(400).json({
-                    status: "failed",
-                    message: "Unable to save the data to database"
-                })
-            }
-
-            res.status(200).json({
+            return res.status(200).json({
                 status: "Successs",
                 message: " Successfully saved to database",
                 data: newReview
             })
+            
         } catch (error) {
             console.log(error)
             return res.status(500).json({
@@ -180,21 +101,22 @@ module.exports = {
             })
         }
     },
+
     getReviews: async (req, res) => {
         try {
             const content = await Reviews.findAll({
                 attributes: {
-                    exclude: ["id", "updatedAt", "createAt"]
+                    exclude: ["updatedAt", "createAt"]
                 },
-                include: [
-                    {
-                        as: "user",
-                        model: Users,
-                        attributes: ['fullname', 'img']
-                    },
-                ],
-                offset: (15 * (page - 1)) + 1,
-                limit: 15
+                // include: [
+                //     {
+                //         as: "user",
+                //         model: Users,
+                //         attributes: ['fullname', 'img']
+                //     },
+                // ],
+                // offset: (15 * (page - 1)) + 1,
+                // limit: 15
             })
             if (!content) {
                 return res.status(200).json({
@@ -217,6 +139,7 @@ module.exports = {
             })
         }
     },
+
     getReview: async (req, res) => {
         try {
             const content = await Reviews.findOne({
@@ -224,15 +147,15 @@ module.exports = {
                     id: req.params.id
                 },
                 attributes: {
-                    exclude: ["id", "updatedAt", "createAt"]
+                    exclude: ["updatedAt", "createAt"]
                 },
-                include: [
-                    {
-                        as: "user",
-                        model: Users,
-                        attributes: ['fullname', 'img']
-                    },
-                ],
+                // include: [
+                //     {
+                //         as: "user",
+                //         model: Users,
+                //         attributes: ['fullname', 'img']
+                //     },
+                // ],
             })
             if (!content) {
                 return res.status(200).json({
@@ -255,56 +178,55 @@ module.exports = {
             })
         }
     },
-    updateReview: async (req, res) => {
+
+    updateReview : async (req, res) => {
         const body = req.body
+        const id = req.params.id
         try {
             const schema = Joi.object({
-                comment: Joi.string().required(),
-                rating: Joi.number().required(),
-            });
+                rating: Joi.number().min(1).max(5).required(),
+                comment: Joi.string()
+            })
 
-            const { error } = schema.validate({ ...body }, { abortEarly: false });
+            const {error} = schema.validate({ ...body }, { abortEarly: false });
 
             if (error) {
                 return res.status(400).json({
-                    status: "failed",
-                    message: "Bad Request",
-                    errors: error["details"][0]["message"]
+                    status : "failed",
+                    message : "Bad Request",
+                    errors : check.error["details"].map(({ message }) => message )
                 })
             }
-            const updatedReview = await Reviews.update({ ...body }, {
-                where: {
-                    id: req.params.id
-                }
-            });
+            
+            const reviewUpdate = await Movies.update(
+                { where : { id } },
 
-            if (!updatedReview[0]) {
+                {
+                    rating: body.rating,
+                    comment: body.comment
+                }
+            ); 
+
+            if(!reviewUpdate[0]) {
                 return res.status(400).json({
-                    status: "Failed",
-                    message: "Unable to Update Database"
-                })
+                    status : "failed",
+                    message : "You Didn't Change Anything"
+                });
             }
-
-            const data = await Reviews.findOne({
-                where: {
-                    id: req.params.id,
-                }
-            })
-
+            
             return res.status(200).json({
-                status: "Success",
-                message: "Data Update Successfully",
-                data: data
-            })
-
+                status : "success",
+                message : "Succesfully update the Movie",
+                data : data
+            });
         } catch (error) {
-            console.log(error)
             return res.status(500).json({
-                status: "Failed",
-                message: "Internal Server Error"
+                status : "failed",
+                message : "Internal Server Error"
             })
         }
     },
+
     deleteReview: async (req, res) => {
         const id = req.params.id
         try {
--- routes/ratingRoutes.js
+++ routes/ratingRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const review = require('../controllers/reviewsControllers');
-const author = require('../middlewares/authorization');
+// const author = require('../middlewares/authorization');
 const auth = require('../middlewares/authentication');
 
 
 router.get('/', review.getReviews);
 router.get('/:id', review.getReview);
 router.post('/', auth, review.postReview);
-router.delete('/:id', auth, author.authAdmin, review.deleteReview);
-router.put('/:id', auth, review.updateReview);
+router.delete('/delete/:id', auth, review.deleteReview);
+router.put('/update/:id', auth, review.updateReview);
 
 module.exports = router
